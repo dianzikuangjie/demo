b34d06dbba341dec6f37a913bc09cf02ba75ee12
dianzikuangjie <zhouhaigang123@126.com>
2021-06-01 12:38:27 +0900

    111

---
+++ demo/src/Demo01.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author tom
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2021/5/19 15:35
+ * @Copyright © 株式会社多言语系统研究所
+ */
+public class Demo01 {
+    /**
+     *      *
+     *    ***
+     *   *****
+     *  *******
+     * *********
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.printf("111");
+        System.out.printf("222");
+        System.out.printf("333");
+    }
+}
