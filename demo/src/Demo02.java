dc32a47309238c2aacbf0330ffe404f44af3cb27
dianzikuangjie <zhouhaigang123@126.com>
2021-06-01 12:40:26 +0900

    222

---
+++ demo/src/Demo02.java
@@ -0,0 +1,12 @@
+/**
+ * @author tom
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2021/6/1 12:39
+ * @Copyright © 株式会社多言语系统研究所
+ */
+public class Demo02 {
+    public static void main(String[] args) {
+        System.out.println("demo02");
+    }
+}
